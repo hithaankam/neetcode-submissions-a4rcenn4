fefdde7dedb7a3c8c4afe9d07d90bb5bef8e1d1a
Hitha Ankam <150412573+hithaankam@users.noreply.github.com>
2026-05-15 17:40:29 +0530

    Add: meeting-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+        arr = []
+
+        for interval in intervals:
+            arr.append((interval.start, interval.end))
+        arr.sort()
+        min_heap = []
+        ans = 1
+
+        heapq.heappush(min_heap, arr[0][1])
+        for start, end in arr[1:]:
+            if min_heap[0] <= start:
+                heapq.heappop(min_heap)
+            heapq.heappush(min_heap, end)
+            #ans = max(ans, len(min_heap))
+        return len(min_heap)
+
+
